10873641d95d0d6d09a28b18e1b5abf98b7b9494
Liv <110914989+livoszlak@users.noreply.github.com>
2024-05-26 18:19:32 +0200

    Key fix

--- app/components/CocktailsDisplay/CocktailsDisplay.tsx
+++ app/components/CocktailsDisplay/CocktailsDisplay.tsx
@@ -18,7 +18,7 @@ export default function CocktailsDisplay(): JSX.Element {
   const context = useContext(IngredientContext);
 
   if (!context) {
-    throw new Error("no ingredient provider");
+    throw new Error("No ingredient provider");
   }
 
   const { ingredient } = context;
