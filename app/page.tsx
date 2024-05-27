9ce114e20690341f5998dc2b9cb2d60dfe0704ce
Liv <110914989+livoszlak@users.noreply.github.com>
2024-05-27 09:12:56 +0200

    Dependency array fixed on useCocktails

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
 "use client";
-import Image from "next/image";
 import styles from "./page.module.css";
-import { useCocktails } from "./hooks/useCocktails";
-import { Cocktail, Drinks } from "./api/cocktailsApi";
 import IngredientSelect from "./components/IngredientSelect/IngredientSelect";
 
 export default function Home() {
